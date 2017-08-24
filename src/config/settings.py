6cdfae432c6450a400eb6c1bf0d424c9ab0d672f
Memory Clutter <memclutter@gmail.com>
2017-08-24 18:17:14 +0300

    Set STATIC_ROOT +

--- src/config/settings.py
+++ src/config/settings.py
@@ -124,4 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = env('STATIC_ROOT', '/var/www/html')
+STATIC_ROOT = env('STATIC_ROOT')
